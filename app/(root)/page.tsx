485365e1ddd9c233738d3fe197b7202016631e0a
haitham <haitham314s@gmail.com>
2023-07-08 17:15:29 +0400

    login auth page

---
+++ app/(root)/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/Button";
+
+function Home() {
+  return (
+    <div className="p-4">
+      <Button size="default" variant="outline">
+        Admin Dashboard
+      </Button>
+    </div>
+  );
+}
+
+export default Home;
