bfeea6e0820490fc42706bcadb9dadce98c050f9
haitham <haitham314s@gmail.com>
2023-07-12 08:28:38 +0400

    category cell actions

--- app/(dashboard)/[storeId]/(routes)/categories/components/CellAction.tsx
+++ app/(dashboard)/[storeId]/(routes)/categories/components/CellAction.tsx
@@ -30,18 +30,20 @@ function CellAction({ data }: CellActionProps) {
 
   const onCopy = (id: string) => {
     navigator.clipboard.writeText(id);
-    toast.success("Billboard id copied to clipboard");
+    toast.success("Category id copied to clipboard");
   };
 
   const onDelete = async () => {
     try {
       setLoading(true);
-      await axios.delete(`/api/${params.storeId}/billboards/${data.id}`);
+      await axios.delete(`/api/${params.storeId}/categories/${data.id}`);
 
       router.refresh();
-      toast.success("Billboard deleted.");
+      toast.success("Category deleted.");
     } catch (error: any) {
-      toast.error("Make sure you removed all categories using this billboard.");
+      toast.error(
+        "Make sure you removed all products using this category first."
+      );
     } finally {
       setLoading(false);
       setOpen(false);
@@ -75,7 +77,7 @@ function CellAction({ data }: CellActionProps) {
 
           <DropdownMenuItem
             onClick={() =>
-              router.push(`/${params.storeId}/billboards/${data.id}`)
+              router.push(`/${params.storeId}/categories/${data.id}`)
             }
           >
             <Edit className="mr-2 h-4 w-4" />
