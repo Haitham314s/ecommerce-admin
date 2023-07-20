4935b46542b4b55a00542565730b9c6cf89a5719
haitham <haitham314s@gmail.com>
2023-07-21 00:15:35 +0400

    product image gallery

--- store/app/(routes)/product/[productId]/page.tsx
+++ store/app/(routes)/product/[productId]/page.tsx
@@ -1,5 +1,8 @@
 import getProduct from "@/actions/get-product";
 import getProducts from "@/actions/get-products";
+import Info from "@/components/Info";
+import ProductList from "@/components/ProductList";
+import Gallery from "@/components/gallery";
 import Container from "@/components/ui/Container";
 
 interface ProductPageProps {
@@ -19,10 +22,15 @@ async function ProductPage({ params: { productId } }: ProductPageProps) {
       <Container>
         <div className="px-4 py-10 sm:px-6 lg:px-8">
           <div className="lg:grid lg:grid-cols-2 lg:items-center lg:gap-x-8">
-            {/* Gallery */}
-            <div>Gallery</div>
-            <div className="mt-10 px-4 sm:mt-16 sm:px-0 lg:mt-0">Info</div>
+            <Gallery images={product.images} />
+
+            <div className="mt-10 px-4 sm:mt-16 sm:px-0 lg:mt-0">
+              <Info data={product} />
+            </div>
           </div>
+
+          <hr className="my-10" />
+          <ProductList title="Related Items" items={suggestedProducts} />
         </div>
       </Container>
     </div>
