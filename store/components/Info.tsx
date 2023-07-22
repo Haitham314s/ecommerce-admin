7f4214e1beceba875c6bfdedee4e9c11c890d6ef
haitham <haitham314s@gmail.com>
2023-07-22 20:23:11 +0400

    add to cart feature on info component

--- store/components/Info.tsx
+++ store/components/Info.tsx
@@ -4,12 +4,17 @@ import { Product } from "@/types";
 
 import Button from "@/components/ui/Button";
 import Currency from "@/components/ui/Currency";
+import useCart from "@/hooks/useCart";
 
 interface InfoProps {
   data: Product;
 }
 
 function Info({ data }: InfoProps) {
+  const addItem = useCart((state) => state.addItem);
+
+  const onAddToCard = () => addItem(data);
+
   return (
     <div>
       <h1 className="text-3xl font-bold text-gray-900">{data.name}</h1>
@@ -36,7 +41,12 @@ function Info({ data }: InfoProps) {
       </div>
 
       <div className="mt-10 flex items-center gap-x-3">
-        <Button className="flex items-center gap-x-2 text-white">Add to cart</Button>
+        <Button
+          className="flex items-center gap-x-2 text-white"
+          onClick={onAddToCard}
+        >
+          Add to cart
+        </Button>
       </div>
     </div>
   );
